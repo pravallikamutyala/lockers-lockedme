a29722d5270c8da3644748355e20658e7b6a06ce
pravallikamutyala <m.pravallika@gmail.com>
2022-03-25 23:30:45 +0530

    Added documents

---
+++ LockedMe/src/com/lockers/lockedme/main/WelcomePage.java
@@ -0,0 +1,14 @@
+package com.lockers.lockedme.main;
+
+public class WelcomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("            WELCOME TO LOCKERS PVT. LTD.            ");
+		System.out.println("****************************************************");
+		System.out.println("------------DEVELOPER: PRAVALLIKA-------------------");
+		
+
+	}
+
+}
